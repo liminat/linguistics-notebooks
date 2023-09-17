539471cc6b98cbe23a2405d8705f372b8151905c
liminat <180078053+liminat@users.noreply.github.com>
2023-09-17 17:29:21 +0000

    Update `.gitignore`

---
+++ machine_translation/nmt/train_gnmt.py
@@ -0,0 +1,25 @@
+"""
+Google Neural Machine Translation
+=================================
+
+This example shows how to implement the GNMT model with Gluon NLP Toolkit.
+
+@article{wu2016google,
+  title={Google's neural machine translation system:
+   Bridging the gap between human and machine translation},
+  author={Wu, Yonghui and Schuster, Mike and Chen, Zhifeng and Le, Quoc V and
+   Norouzi, Mohammad and Macherey, Wolfgang and Krikun, Maxim and Cao, Yuan and Gao, Qin and
+   Macherey, Klaus and others},
+  journal={arXiv preprint arXiv:1609.08144},
+  year={2016}
+}
+"""
+
+# coding: utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License
