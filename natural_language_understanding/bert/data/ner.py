b9de2d3e388e7a790655f632ec0b6efe12603156
liminat <180078053+liminat@users.noreply.github.com>
2023-09-27 18:54:18 +0000

    dep fix

--- natural_language_understanding/bert/data/ner.py
+++ natural_language_understanding/bert/data/ner.py
@@ -218,4 +218,140 @@ class BERTTaggingDataset(object):
                                                                      self.bert_tokenizer)
         dev_sentences = [] if dev_path is None else load_segment(dev_path, self.bert_tokenizer)
         test_sentences = [] if test_path is None else load_segment(test_path, self.bert_tokenizer)
-        all_s
+        all_sentences = train_sentences + dev_sentences + test_sentences
+
+        if tag_vocab is None:
+            logging.info('Indexing tags...')
+            tag_counter = nlp.data.count_tokens(token.tag
+                                                for sentence in all_sentences for token in sentence)
+            self.tag_vocab = nlp.Vocab(tag_counter, padding_token=NULL_TAG,
+                                       bos_token=None, eos_token=None, unknown_token=None)
+        else:
+            self.tag_vocab = tag_vocab
+        self.null_tag_index = self.tag_vocab[NULL_TAG]
+
+        if len(test_sentences) > 0:
+            logging.info('example test sentences:')
+            for i in range(10):
+                logging.info(str(test_sentences[i]))
+
+        self.train_inputs = [self._encode_as_input(sentence) for sentence in train_sentences]
+        self.dev_inputs = [self._encode_as_input(sentence) for sentence in dev_sentences]
+        self.test_inputs = [self._encode_as_input(sentence) for sentence in test_sentences]
+
+        logging.info('tag_vocab: %s', self.tag_vocab)
+
+    def _encode_as_input(self, sentence):
+        """Enocde a single sentence into numpy arrays as input to the BERTTagger model.
+
+        Parameters
+        ----------
+        sentence: List[TaggedToken]
+            A sentence as a list of tagged tokens.
+
+        Returns
+        -------
+        np.array: token text ids (batch_size, seq_len)
+        np.array: token types (batch_size, seq_len),
+                which is all zero because we have only one sentence for tagging.
+        np.array: valid_length (batch_size,) the number of tokens until [SEP] token
+        np.array: tag_ids (batch_size, seq_len)
+        np.array: flag_nonnull_tag (batch_size, seq_len),
+                which is simply tag_ids != self.null_tag_index
+
+        """
+        # check whether the given sequence can be fit into `seq_len`.
+        assert len(sentence) <= self.seq_len - 2, \
+            'the number of tokens {} should not be larger than {} - 2. offending sentence: {}' \
+            .format(len(sentence), self.seq_len, sentence)
+
+        text_tokens = ([self.text_vocab.cls_token] + [token.text for token in sentence] +
+                       [self.text_vocab.sep_token])
+        padded_text_ids = (self.text_vocab.to_indices(text_tokens)
+                           + ([self.text_vocab[self.text_vocab.padding_token]]
+                              * (self.seq_len - len(text_tokens))))
+
+        tags = [NULL_TAG] + [token.tag for token in sentence] + [NULL_TAG]
+        padded_tag_ids = (self.tag_vocab.to_indices(tags)
+                          + [self.tag_vocab[NULL_TAG]] * (self.seq_len - len(tags)))
+
+        assert len(text_tokens) == len(tags)
+        assert len(padded_text_ids) == len(padded_tag_ids)
+        assert len(padded_text_ids) == self.seq_len
+
+        valid_length = len(text_tokens)
+
+        # in sequence tagging problems, only one sentence is given
+        token_types = [0] * self.seq_len
+
+        np_tag_ids = np.array(padded_tag_ids, dtype='int32')
+        # gluon batchify cannot batchify numpy.bool? :(
+        flag_nonnull_tag = (np_tag_ids != self.null_tag_index).astype('int32')
+
+        return (np.array(padded_text_ids, dtype='int32'),
+                np.array(token_types, dtype='int32'),
+                np.array(valid_length, dtype='int32'),
+                np_tag_ids,
+                flag_nonnull_tag)
+
+    @staticmethod
+    def _get_data_loader(inputs, shuffle, batch_size):
+        return mx.gluon.data.DataLoader(inputs, batch_size=batch_size, shuffle=shuffle,
+                                        last_batch='keep')
+
+    def get_train_data_loader(self, batch_size):
+        return self._get_data_loader(self.train_inputs, shuffle=True, batch_size=batch_size)
+
+    def get_dev_data_loader(self, batch_size):
+        return self._get_data_loader(self.dev_inputs, shuffle=False, batch_size=batch_size)
+
+    def get_test_data_loader(self, batch_size):
+        return self._get_data_loader(self.test_inputs, shuffle=False, batch_size=batch_size)
+
+    @property
+    def num_tag_types(self):
+        """Returns the number of unique tags.
+
+        Returns
+        -------
+        int: number of tag types.
+        """
+        return len(self.tag_vocab)
+
+
+def convert_arrays_to_text(text_vocab, tag_vocab,
+                           np_text_ids, np_true_tags, np_pred_tags, np_valid_length):
+    """Convert numpy array data into text
+
+    Parameters
+    ----------
+    np_text_ids: token text ids (batch_size, seq_len)
+    np_true_tags: tag_ids (batch_size, seq_len)
+    np_pred_tags: tag_ids (batch_size, seq_len)
+    np.array: valid_length (batch_size,) the number of tokens until [SEP] token
+
+    Returns
+    -------
+    List[List[PredictedToken]]:
+
+    """
+    predictions = []
+    for sample_index in range(np_valid_length.shape[0]):
+        sample_len = np_valid_length[sample_index]
+        entries = []
+        for i in range(1, sample_len - 1):
+            token_text = text_vocab.idx_to_token[np_text_ids[sample_index, i]]
+            true_tag = tag_vocab.idx_to_token[int(np_true_tags[sample_index, i])]
+            pred_tag = tag_vocab.idx_to_token[int(np_pred_tags[sample_index, i])]
+            # we don't need to predict on NULL tags
+            if true_tag == NULL_TAG:
+                last_entry = entries[-1]
+                entries[-1] = PredictedToken(text=last_entry.text + token_text,
+                                             true_tag=last_entry.true_tag,
+                                             pred_tag=last_entry.pred_tag)
+            else:
+                entries.append(PredictedToken(text=token_text,
+                                              true_tag=true_tag, pred_tag=pred_tag))
+
+        predictions.append(entries)
+    return predictions
