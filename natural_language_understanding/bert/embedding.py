aea037cc6b6ca264093d8e8fe1ece282a87e0fb8
liminat <180078053+liminat@users.noreply.github.com>
2023-10-02 18:18:35 +0000

    refactor spec

--- natural_language_understanding/bert/embedding.py
+++ natural_language_understanding/bert/embedding.py
@@ -90,4 +90,136 @@ class BertEmbedding(object):
                                                max_seq_length=self.max_seq_length,
                                                pair=False)
 
-    def __call__(self, sentences, oov_way='avg')
+    def __call__(self, sentences, oov_way='avg'):
+        return self.embedding(sentences, oov_way='avg')
+
+    def embedding(self, sentences, oov_way='avg'):
+        """
+        Get tokens, tokens embedding
+
+        Parameters
+        ----------
+        sentences : List[str]
+            sentences for encoding.
+        oov_way : str, default avg.
+            use **avg**, **sum** or **last** to get token embedding for those out of
+            vocabulary words
+
+        Returns
+        -------
+        List[(List[str], List[ndarray])]
+            List of tokens, and tokens embedding
+        """
+        data_iter = self.data_loader(sentences=sentences)
+        batches = []
+        for token_ids, valid_length, token_types in data_iter:
+            token_ids = token_ids.as_in_context(self.ctx)
+            valid_length = valid_length.as_in_context(self.ctx)
+            token_types = token_types.as_in_context(self.ctx)
+            sequence_outputs = self.bert(token_ids, token_types,
+                                         valid_length.astype(self.dtype))
+            for token_id, sequence_output in zip(token_ids.asnumpy(),
+                                                 sequence_outputs.asnumpy()):
+                batches.append((token_id, sequence_output))
+        return self.oov(batches, oov_way)
+
+    def data_loader(self, sentences, shuffle=False):
+        """Load, tokenize and prepare the input sentences."""
+        dataset = BertEmbeddingDataset(sentences, self.transform)
+        return DataLoader(dataset=dataset, batch_size=self.batch_size, shuffle=shuffle)
+
+    def oov(self, batches, oov_way='avg'):
+        """
+        How to handle oov. Also filter out [CLS], [SEP] tokens.
+
+        Parameters
+        ----------
+        batches : List[(tokens_id,
+                        sequence_outputs,
+                        pooled_output].
+            batch   token_ids (max_seq_length, ),
+                    sequence_outputs (max_seq_length, dim, ),
+                    pooled_output (dim, )
+        oov_way : str
+            use **avg**, **sum** or **last** to get token embedding for those out of
+            vocabulary words
+
+        Returns
+        -------
+        List[(List[str], List[ndarray])]
+            List of tokens, and tokens embedding
+        """
+        sentences = []
+        for token_ids, sequence_outputs in batches:
+            tokens = []
+            tensors = []
+            oov_len = 1
+            for token_id, sequence_output in zip(token_ids, sequence_outputs):
+                if token_id == 1:
+                    # [PAD] token, sequence is finished.
+                    break
+                if token_id in (2, 3):
+                    # [CLS], [SEP]
+                    continue
+                token = self.vocab.idx_to_token[token_id]
+                if token.startswith('##'):
+                    token = token[2:]
+                    tokens[-1] += token
+                    if oov_way == 'last':
+                        tensors[-1] = sequence_output
+                    else:
+                        tensors[-1] += sequence_output
+                    if oov_way == 'avg':
+                        oov_len += 1
+                else:  # iv, avg last oov
+                    if oov_len > 1:
+                        tensors[-1] /= oov_len
+                        oov_len = 1
+                    tokens.append(token)
+                    tensors.append(sequence_output)
+            if oov_len > 1:  # if the whole sentence is one oov, handle this special case
+                tensors[-1] /= oov_len
+            sentences.append((tokens, tensors))
+        return sentences
+
+
+if __name__ == '__main__':
+    np.set_printoptions(threshold=5)
+    parser = argparse.ArgumentParser(description='Get embeddings from BERT',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--gpu', type=int, default=None,
+                        help='id of the gpu to use. Set it to empty means to use cpu.')
+    parser.add_argument('--dtype', type=str, default='float32', help='data dtype')
+    parser.add_argument('--model', type=str, default='bert_12_768_12',
+                        help='pre-trained model')
+    parser.add_argument('--dataset_name', type=str, default='book_corpus_wiki_en_uncased',
+                        help='dataset')
+    parser.add_argument('--params_path', type=str, default=None,
+                        help='path to a params file to load instead of the pretrained model.')
+    parser.add_argument('--max_seq_length', type=int, default=25,
+                        help='max length of each sequence')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='batch size')
+    parser.add_argument('--oov_way', type=str, default='avg',
+                        help='how to handle oov\n'
+                             'avg: average all oov embeddings to represent the original token\n'
+                             'sum: sum all oov embeddings to represent the original token\n'
+                             'last: use last oov embeddings to represent the original token\n')
+    parser.add_argument('--sentences', type=to_unicode, nargs='+', default=None,
+                        help='sentence for encoding')
+    parser.add_argument('--file', type=str, default=None,
+                        help='file for encoding')
+    parser.add_argument('--verbose', action='store_true', help='verbose logging')
+    args = parser.parse_args()
+
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.getLogger().setLevel(level)
+    logging.info(args)
+
+    if args.gpu is not None:
+        context = mx.gpu(args.gpu)
+    else:
+        context = mx.cpu()
+    bert_embedding = BertEmbedding(ctx=context, model=args.model, dataset_name=args.dataset_name,
+                                   max_seq_length=args.max_seq_length, batch_size=args.batch_size)
+    
