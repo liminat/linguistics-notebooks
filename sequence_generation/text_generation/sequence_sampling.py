898be68dc4a010190d495b5751269833514e441f
liminat <180078053+liminat@users.noreply.github.com>
2024-06-29 17:36:27 +0000

    Regenerated gemspec for version 0.5.1

---
+++ sequence_generation/text_generation/sequence_sampling.py
@@ -0,0 +1,21 @@
+"""
+Generate Sentences by Sampling and Beam Search
+==============================================
+
+This example shows how to load a pre-trained language model on wikitext-2 in Gluon NLP Toolkit model
+zoo, and use sequence sampler and beam search sampler on the language model to generate sentences.
+"""
+
+# coding: utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writin
